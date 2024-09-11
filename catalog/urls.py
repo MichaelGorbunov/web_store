e8ffe91ab7580e297a66e3577730c5739d959cb0
Michael Gorbunov <mg0rby@yandex.ru>
2024-09-11 14:26:17 +0300

    описание

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import include, path
 
-from catalog import views
+# from catalog import views
 from catalog.apps import CatalogConfig
+from catalog import views
+from catalog.views import contact
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("home/", views.home, name="home"),
-    path("contact/", views.contact, name="contact"),
+    path("contact/", contact, name="contact"),
 ]
