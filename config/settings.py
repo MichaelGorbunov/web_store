19c74d0878c7e2ec44ed9915f6cb2a3eb9e71d2c
Michael Gorbunov <mg0rby@yandex.ru>
2024-09-11 09:31:47 +0300

    static add

--- config/settings.py
+++ config/settings.py
@@ -116,6 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    # Здесь вы можете добавить пути к вашим статическим файлам,
+    # если они располагаются вне приложений
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
