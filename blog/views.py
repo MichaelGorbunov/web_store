c247f90250bcf93c48ea43e2919b95e44e70dbca
Michael Gorbunov <mg0rby@yandex.ru>
2024-10-02 14:39:09 +0300

    Увеличение счетчика просмотров

--- blog/views.py
+++ blog/views.py
@@ -14,18 +14,33 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/posts_list.html'
     context_object_name = 'posts'
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.order_by("-id")
 
 
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
+    def get_object(self):
+        # Переопределение метода get_object для настройки логики выбора объекта
+        obj = super().get_object()
+        # Дополнительная логика (например,  изменения значений полей)
+        # if not obj.is_active:
+        #     raise Http404("Object not found")
+        obj.views_count=obj.views_count+1
+        obj.save()
+        return obj
+
+
+
 
 
 
 class PostUpdateView(UpdateView):
     model = Post
-    fields = ["title","body"]
+    fields = ["title","body","published","views_count"]
     template_name = 'blog/post_form.html'
     success_url = reverse_lazy('blog:posts_list')
 # Create your views here.
