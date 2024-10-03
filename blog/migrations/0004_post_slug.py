d71c175e52dd2de2d9e0217a0237e8b5ab8cdf18
Michael Gorbunov <mg0rby@yandex.ru>
2024-10-03 17:30:33 +0300

    миграции конфликт

--- blog/migrations/0004_post_slug.py
+++ blog/migrations/0004_post_slug.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("blog", "0002_alter_post_preview"),
+        ("blog", "0003_alter_post_views_count"),
     ]
 
     operations = [
--- blog/migrations/0005_alter_post_slug.py
+++ blog/migrations/0005_alter_post_slug.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("blog", "0003_post_slug"),
+        ("blog", "0004_post_slug"),
     ]
 
     operations = [
