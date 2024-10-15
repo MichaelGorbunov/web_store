5bd102948d20bfb89c73b10fc39d089d0e298b0e
Michael Gorbunov <mg0rby@yandex.ru>
2024-10-15 11:16:35 +0300

    валидация png jp g 5MB

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,14 +10,18 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
 
-    path("", ProductsListView.as_view(), name="product_list"),
+    path("catalog/", ProductsListView.as_view(), name="product_list"),
+    path("", ProductModListView.as_view(), name="product_mod_list"),
+
     path("catalog/product/all", ProductsListView.as_view(), name="products_list"),
     path("catalog/category/all", CategoryesListView.as_view(), name="categoryes_list"),
     path("catalog/contact/", ContactListView.as_view(), name="contact"),
     path("catalog/product_detail/<int:pk>/", ProductDetailView.as_view(), name='product_detail'),
-    path("catalog/product/", ProductCreateView.as_view(), name="product_create"),
+
     path("catalog/category/", CategoryCreateView.as_view(), name="category_mod_list"),
     path("catalog/category/<int:pk>/", CategoryUpdateView.as_view(), name="category_mod"),
+
+    path("catalog/product/", ProductCreateView.as_view(), name="product_create"),
     path("catalog/product/<int:pk>/", ProductUpdateView.as_view(), name="product_mod"),
     path('catalog/product_mod_list/', ProductModListView.as_view(), name='product_mod_list'),
     path("catalog/product_mod_detail/<int:pk>/", ProductModDetailView.as_view(), name='product_mod_detail'),
